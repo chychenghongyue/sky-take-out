02e8e6976889994848c055807bd1111712bf6804
chy <3319023171@qq.com>
2023-08-22 01:07:41 +0800

    菜品模块

--- sky-common/src/main/java/com/sky/properties/MinioProperties.java
+++ sky-common/src/main/java/com/sky/properties/MinioProperties.java
@@ -1,7 +1,9 @@
 package com.sky.properties;
 
+import io.minio.MinioClient;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,5 +14,14 @@ public class MinioProperties {
     private String accessKey;
     private String secretKey;
     private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
 
 }
